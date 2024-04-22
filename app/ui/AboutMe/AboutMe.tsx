63a914330f03c408a659b5e6b6846359c48c10b8
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-04-22 12:05:36 +0300

    add

--- app/ui/AboutMe/AboutMe.tsx
+++ app/ui/AboutMe/AboutMe.tsx
@@ -23,9 +23,7 @@ const AboutMe = () => {
       />
 
       <Text
-        text="I have good knowledge of HTML, CSS, JavaScript, React and NodeJS. In
-          addition, I have hands-on experience with Agile/Scrum. course work
-          methodology"
+        text="Dynamic FullStack Developer with a solid foundation in web development technologies such as HTML5, CSS3, SCSS, and GIT, coupled with expertise in JavaScript, React, Redux, NextJs, Tailwind, Node, Express, and TypeScript. Passionate about crafting efficient and user-friendly web applications, with a keen eye for detail and a commitment to delivering high-quality results. Proven ability to collaborate effectively within a team environment, utilizing strong communication and problem-solving skills. Known for being conscientious, calm, and punctual, with a relentless drive to learn and adapt to new technologies. Seeking a full-time position where I can leverage my skills and continue to grow professionally."
         className={style.about}
       />
       <a href="/public/cv.pdf" download className={style.downloadLink}>
